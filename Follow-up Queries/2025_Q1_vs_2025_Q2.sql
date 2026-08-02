9473c4baac7fc66d2a437559a1cd50cf84c43184
chasetdlockhart <chasetdlockhart@gmail.com>
2026-08-01 16:29:19 -0800

    Follow up research

---
+++ Follow-up Queries/2025_Q1_vs_2025_Q2.sql
@@ -0,0 +1,22 @@
+with cte_order_quarters as(
+	select
+		date_trunc('quarter', soh.orderdate)::date as order_quarter,
+		sum(sod.unitprice * sod.orderqty * (1 - sod.unitpricediscount)) as revenue,
+		count(distinct soh.salesorderid) as order_count,
+		count(distinct soh.customerid) as customer_count
+	from sales.salesorderheader soh
+	join sales.salesorderdetail sod
+		on soh.salesorderid = sod.salesorderid
+	where soh.orderdate >= '2025-01-01'
+	group by date_trunc('quarter', soh.orderdate)::date
+)
+select
+	order_quarter,
+	round(revenue::numeric) as revenue,
+	order_count,
+	customer_count,
+	round(revenue::numeric / order_count) as average_order_value,
+	round(revenue::numeric / customer_count) as average_revenue_per_customer
+from cte_order_quarters;
+
+	
