01ff8d9fd86bd3f20388d3c3d0a537182d6a5ef9
chasetdlockhart <chasetdlockhart@gmail.com>
2026-08-04 11:09:42 -0800

    Removed the online/offline orders from churn analysis

--- Initial Queries/churn_analysis.sql
+++ Initial Queries/churn_analysis.sql
@@ -45,11 +45,3 @@ select
 from cte_new_retained_reactivated_churned
 group by order_quarter
 order by order_quarter;
-
-select
-	date_trunc('quarter', orderdate)::date as order_quarter,
-	count(*) filter (where onlineorderflag = true) as online_orders,
-	count(*) filter (where onlineorderflag = false) as offline_orders
-from sales.salesorderheader
-group by date_trunc('quarter', orderdate)::date
-order by date_trunc('quarter', orderdate)::date;
